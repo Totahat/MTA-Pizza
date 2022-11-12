e3d5fa5a16c09c1265cdf370c03e130bd84118f8
Totahat <yakirtwil@gmail.com>
2022-11-13 00:40:56 +0200

    first commit

--- MTA Pizza/Pizza.c
+++ MTA Pizza/Pizza.c
@@ -1,5 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define pizzasize 40
+#define pizzaprice 60.00
+#define deliveryprice 15
+#define oliveprice 10
+#define shroomprice 12 
 
 void main() {
 	int ID;
@@ -12,10 +17,10 @@ void main() {
 	scanf("%d", &ID);
 	printf("\nOur menu:\n");
 	printf("*********\n");
-	printf("Basic pizza: 60.00 NIS for 40x40 size pizza\n");
-	printf("Toppings for basic  size pizza:\n");
-	printf("Olives: 10 NIS\n");
-	printf("Mushrooms: 12 NIS\n\n");
+	printf("Basic pizza: %.2f NIS for %dx%d size pizza\n", pizzaprice, pizzasize, pizzasize);
+	printf("Toppings for basic size pizza:\n");
+	printf("Olives: %d NIS\n", oliveprice);
+	printf("Mushrooms: %d NIS\n\n", shroomprice);
 	printf("Please enter your pizza's length (cm):");
 	scanf("%d", &length);
 	printf("\nPlease enter your pizza's width (cm):");
@@ -25,23 +30,23 @@ void main() {
 	printf("0.5 for half\n");
 	printf("0.25 for quarter\n");
 	printf("or 0 for none:");
-	scanf("%f", &olive);
+	scanf(" %f", &olive);
 	printf("\nMushrooms - Enter 1 for the whole pizza\n");
 	printf("0.5 for half\n");
 	printf("0.25 for quarter\n");
 	printf("or 0 for none:");
-	scanf("%f", &shroom);
-	printf("\nDo you want delivery for the price of 15 NIS? Enter 1 for delivery or 0 for pick-up: ");
+	scanf(" %f", &shroom);
+	printf("\nDo you want delivery for the price of %d NIS? Enter 1 for delivery or 0 for pick-up: ", deliveryprice);
 	scanf("%d", &delivery);
-	pizzap = (float)(length * width) / 1600 * 60;
-	olivep = (length * width*olive) / 1600 * 10;
-	shroomp= (length * width * shroom) / 1600 * 12;
-	price = pizzap + olivep + shroomp+delivery*15;
+	pizzap = (float)(length * width) / (pizzasize * pizzasize) * pizzaprice;
+	olivep = (length * width * olive) / (pizzasize * pizzasize) * oliveprice;
+	shroomp= (length * width * shroom) / (pizzasize * pizzasize) * shroomprice;
+	price = pizzap + olivep + shroomp+delivery* deliveryprice;
 	pricetax = (int)(price * 1.17);
 	printf("\nYour order details:\n");
 	printf("*******************\n");
 	printf("ID number: %09d\n", ID);
-	printf("Pizza size: %dX%d\n", length, width);
+	printf("Pizza size: %dx%d\n", length, width);
 	printf("Total price: %.2f\n", price);
 	printf("Total price with tax (rounded down): %d\n", pricetax);
 	printf("Thank you for your order!");
